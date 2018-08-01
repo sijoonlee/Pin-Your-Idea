51e0287c59b0a3474563b7e7a4c41d9c3e5b5f9b
SIJOON LEE <shijoonlee@gmail.com>
2018-08-02 08:51:29 +0900

    lots

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 (function(window){
 	'use strict'
 
+//
+// pinBoard Array
+// [ { id: , title: , content: }, { id: , title: , content: }, ... ]
+// memo = {id:, title:, content:}
+//
     class Store {
         constructor(){
         this.pinBoard = [ {id: "default", title: "Your First memo", content: "Write Something!"} ];
@@ -9,9 +14,6 @@
 	}
     Store.prototype.savePinBoard = function (pinBoard){
 		this.pinBoard = pinBoard;
-	}
-    Store.prototype.loadPinBoard = function (){
-        return this.pinBoard;
 	}
     /*FILE I/O
     Store.prototype.readpinBoard = function (pinBoard){
@@ -41,18 +43,26 @@ console.log(model.pinBoard);
 
 
     class Model {
-        constructor(store) {
-            this.store = store;
-            this.pinBoard = Array.from(this.store.loadPinBoard(), x=>x);
+        constructor(pinBoard) {
+            this.pinBoard = []; // new Array container
+            for (var i in pinBoard){
+                this.pinBoard[i] = Object.assign({}, pinBoard[i]);
+            } // Clone Object
+
+            //this.pinBoard = Array.from(pinBoard, x=>x);
+            //this.pinBoard = pinBoard.slice(0);
             //clone store.pinBoard to model.pinBoard
-            //Also Possible: this.pinBoard = this.store.loadPinBoard().slice(0);
+            //Also Possible: this.pinBoard = this.loadPinBoard().slice(0);
             //Object Clone: var x = {myProp: "value"}; var y = Object.assign({}, x);
-            this.autoSave = false;
         }
     }
 
 	Model.prototype.generateId = function(memo){
 		memo.id = "memo-" + Date.now();
+	}
+
+    Model.prototype.undoPinBoard = function (pinBoard){
+        this.pinBoard = pinBoard;
 	}
 
 	Model.prototype.addMemo = function (memo){
@@ -61,7 +71,7 @@ console.log(model.pinBoard);
 		this.pinBoard.push(memo);
 		}
 		else return false;
-        if(this.autoSave) this.store.savePinBoard(this.pinBoard);
+        if(this.autoSave) this.savePinBoard(this.pinBoard);
 	}
 
 	Model.prototype.findMemo = function (memo){
@@ -70,22 +80,33 @@ console.log(model.pinBoard);
 		}
 		return false;
 	}
+    
+    /*
 	Model.prototype.updateMemo = function (memo){
 		if ( this.findMemo(memo) === false ) return false;
 		this.findMemo(memo).title = memo.title;
 		this.findMemo(memo).content = memo.content;
-        if(this.autoSave) this.store.savePinBoard(this.pinBoard);
-	}
+    }
+    */
+    
+    Model.prototype.updateMemo = function (type, id, txt){
+        switch(type){
+            case "title":
+                for( var i in this.pinBoard ){
+                    if(id == this.pinBoard[i].id) this.pinBoard[i].title = txt;
+                }
+                break;
+            case "content":
+                for( var i in this.pinBoard ){
+                    if(id == this.pinBoard[i].id) this.pinBoard[i].content = txt;
+                }
+                break;
+        }
+    }
+                
 	Model.prototype.deleteMemo = function (memo){
 		this.pinBoard.splice(this.pinBoard.indexOf(this.findMemo(memo)),1);
-        if(this.autoSave) this.store.savePinBoard(this.pinBoard);
 	}
-    Model.prototype.toggleAutoSave = function(){
-        if(this.autoSave) this.autoSave = false;
-        else this.autoSave = true;
-    }
-
-
 
 	//export to window
 	window.app = window.app || {};
@@ -107,31 +128,53 @@ document.getElementById("editor").addEventListener("input", function() {
 
     class View {
         constructor(pinBoard) {
-            this.pinBoard = pinBoard;
+            this.setView(pinBoard);
         }
     }
-	View.prototype.setView = function (pinBoard){
+
+    View.prototype.collectNodeArray = function (pinBoard){
         var nodeArray = [];
         for ( var i in pinBoard ){
 			var memoNode = document.getElementById(pinBoard[i].id);
-            var titleNode = memoNode.getElementsByClassName("title")[0]
-            var contentNode = memoNode.getElementsByClassName("content")[0]
-			titleNode.innerHTML = pinBoard[i].title;
-            titleNode.contentEditable = true;
-            contentNode.innerHTML = pinBoard[i].content;
-            contentNode.contentEditable = true;
-            nodeArray.push({id:pinBoard[i].id, title:titleNode, content:contentNode});
+            var titleNode = memoNode.getElementsByClassName("title")[0];
+            var contentNode = memoNode.getElementsByClassName("content")[0];
+            var deleteButtonNode = memoNode.getElementsByClassName("delete")[0];
+            var hideButtonNode = memoNode.getElementsByClassName("hide")[0];
+            nodeArray.push({id:pinBoard[i].id, title:titleNode, content:contentNode, deleteButton:deleteButtonNode, hideButton:hideButtonNode});
         }
         return nodeArray;
     }
 
-	View.prototype.addMemo = function (memo){
+    View.prototype.setView = function (pinBoard){
+        var nodeArray = this.collectNodeArray(pinBoard);
+        for (var i in nodeArray){
+            var memoNode = nodeArray[i];
+            memoNode.title.innerHTML = pinBoard[i].title;
+            memoNode.title.contentEditable = true;
+            memoNode.content.innerHTML = pinBoard[i].content;
+            memoNode.content.contentEditable = true;
+        }
+    }
+    View.prototype.toggleHide = function (node){
+        var title = node.title;
+        var content = node.content;
+        if(content.style.display == 'none'){
+            title.style.display = 'block'
+            content.style.display = 'block';
+        } else {
+            title.style.display = 'none'
+            content.style.display = 'none';
+        }
         
+    }
+        
+
+
+	View.prototype.addMemo = function (memo){  
 	}
 	View.prototype.findMemo = function (memo){
 	}
 	View.prototype.updateMemo = function (memo){
-
 	}
 	View.prototype.deleteMemo = function (memo){
 	}
@@ -155,44 +198,57 @@ document.getElementById("editor").addEventListener("input", function() {
 (function(window){
 	'use strict'
     class Controller {
-        constructor(model, view) {
+        constructor(model, view, store) {
             this.model = model;
             this.view = view;
-            this.setView(model.pinBoard);
+            this.store = store;
+            this.autoSave = false;
+            this.bindEvent(this.model.pinBoard);
         }
     }
-	Controller.prototype.addMemo = function (memo){
+    Controller.prototype.toggleAutoSave = function(){
+        if(this.autoSave) this.autoSave = false;
+        else this.autoSave = true;
     }
-	Controller.prototype.updateMemo = function (id,memo){
-	}
+    
+	Controller.prototype.addMemo = function (memo){
+        
+    }
+    Controller.prototype.updateMemo = function (type, id, node){
+        this.model.updateMemo(type, id, node.innerHTML);
+        if(this.autoSave) this.store.savePinBoard(this.model.pinBoard);        
+    }    
+	
+    Controller.prototype.bindEvent = function (pinBoard){
+        var nodeArray = this.view.collectNodeArray(pinBoard);
+        for (let i in nodeArray){
+            var id = nodeArray[i].id;
+            var titleNode = nodeArray[i].title;
+            var contentNode = nodeArray[i].content;
+            var hideButtonNode = nodeArray[i].hideButton;
+            titleNode.addEventListener("input", this.updateMemo.bind(this, "title", id, titleNode));
+            contentNode.addEventListener("input", this.updateMemo.bind(this, "content", id, contentNode));
+            //nodeArray[i].deleteButton.addEventListener("click", this.deleteMemo);
+            hideButtonNode.addEventListener("click", this.view.toggleHide.bind(this, nodeArray[i]));
+        }
+    }    
+    
+    
+        
+    
+
+    Controller.prototype.undoPinBoard = function (){
+        this.model.pinBoard = this.store.pinBoard;
+        this.setView(this.model.pinBoard);
+    }
 	Controller.prototype.deleteMemo = function (id){
 	}
 	Controller.prototype.readMemo = function (id){
 	}
 	Controller.prototype.writeMemo = function (id){
 	}
-    Controller.prototype.setView = function (pinBoard){
-        this.bindEvent(this.view.setView(pinBoard));
-    }
-    Controller.prototype.bindEvent = function (nodeArray){
-        
-        for (let i in nodeArray){
-            var id = nodeArray[i].id;
-            var titleNode = nodeArray[i].title;
-            var contentNode = nodeArray[i].content;
-            nodeArray[i].title.addEventListener("input", this.titleInputDetected.bind(this, id, titleNode));
-            //nodeArray[i].content.addEventListener("input", this.inputDetected(nodeArray[i].id,nodeArray[i].content));
-        }
-    };
-    Controller.prototype.titleInputDetected = function (id, titleNode){
-        console.log(id);
-        console.log(titleNode);
-    }
-    Controller.prototype.contentInputDetected = function (id, contentNode){
-        console.log(id);
-        console.log(this);
-    }
-    
+
+
 	//export to window
 	window.app = window.app || {};
 	window.app.Controller = Controller;
@@ -206,8 +262,8 @@ document.getElementById("editor").addEventListener("input", function() {
 (function(window){
 	'use strict'
     window.store = new app.Store;
-	window.model = new app.Model(store);
+	window.model = new app.Model(store.pinBoard);
 	window.view = new app.View(model.pinBoard);
-	window.controller = new app.Controller(model, view);
+	window.controller = new app.Controller(model, view, store);
 
 })(window);
