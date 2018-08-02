a9f893d1b03a1c2a27ef4d7649f6b3351cd377e5
SIJOON LEE <shijoonlee@gmail.com>
2018-08-02 22:06:11 +0900

    complete basic features

--- js/app.js
+++ js/app.js
@@ -1,269 +1,199 @@
-//Store
-(function(window){
-	'use strict'
 
-//
-// pinBoard Array
-// [ { id: , title: , content: }, { id: , title: , content: }, ... ]
-// memo = {id:, title:, content:}
-//
-    class Store {
-        constructor(){
-        this.pinBoard = [ {id: "default", title: "Your First memo", content: "Write Something!"} ];
-        }
+(function(window){
+	'use strict' // IIFE + "use strict" prevent scope problems
+	class Store {
+	    constructor(pinBoard){
+			var defaultPinBoard = [ {id: "memo-default", title: "", content: ""} ];
+	        this.pinBoard = pinBoard || defaultPinBoard;
+	    }
 	}
-    Store.prototype.savePinBoard = function (pinBoard){
-		this.pinBoard = pinBoard;
+	Store.prototype.savePinBoard = function (pinBoard){
+			this.pinBoard = pinBoard;
 	}
-    /*FILE I/O
-    Store.prototype.readpinBoard = function (pinBoard){
-	}
-    Store.prototype.writepinBoard = function (pinBoard){
-	}
-    */
-    window.app = window.app || {};
+	// export to window
+	window.app = window.app || {};
 	window.app.Store = Store;
 })(window);
-
-//model
-/* test code
-
-var memoA = {id:"default", title:"new title", content:"new new"};
-var memoB = {title:"abc title", content:"abc"};
-model.addMemo(memoB);
-console.log(model.pinBoard)
-console.log(model.findMemo(memoB));
-model.updateMemo(memoA);
-console.log(model.pinBoard)
-model.deleteMemo(memoB);
-console.log(model.pinBoard);
-*/
+//MODEL
 (function(window){
 	'use strict'
-
-
-    class Model {
-        constructor(pinBoard) {
-            this.pinBoard = []; // new Array container
-            for (var i in pinBoard){
-                this.pinBoard[i] = Object.assign({}, pinBoard[i]);
-            } // Clone Object
-
-            //this.pinBoard = Array.from(pinBoard, x=>x);
-            //this.pinBoard = pinBoard.slice(0);
-            //clone store.pinBoard to model.pinBoard
-            //Also Possible: this.pinBoard = this.loadPinBoard().slice(0);
-            //Object Clone: var x = {myProp: "value"}; var y = Object.assign({}, x);
-        }
-    }
-
-	Model.prototype.generateId = function(memo){
-		memo.id = "memo-" + Date.now();
-	}
-
-    Model.prototype.undoPinBoard = function (pinBoard){
-        this.pinBoard = pinBoard;
-	}
-
-	Model.prototype.addMemo = function (memo){
-		if(this.findMemo(memo)===false){
-		this.generateId(memo);
-		this.pinBoard.push(memo);
+	//Array Clone
+	//this.pinBoard = Array.from(pinBoard, x=>x);
+	//this.pinBoard = pinBoard.slice(0);
+	//Object Clone
+	//var x = {myProp: "value"}; var y = Object.assigx);
+	class Model {
+		constructor(pinBoard) {
+		    this.pinBoard = []; // new Array container
+		    for (var i in pinBoard){
+		        this.pinBoard[i] = Object.assign({}, pinBoard[i]);
+		    } // Clone Object
 		}
-		else return false;
-        if(this.autoSave) this.savePinBoard(this.pinBoard);
 	}
-
-	Model.prototype.findMemo = function (memo){
+	Model.prototype.addMemo = function (memo){
+		this.pinBoard.push(memo);
+	}
+	Model.prototype.findMemo = function (id){
 		for ( var i in this.pinBoard) {
-			if ( this.pinBoard[i].id === memo.id ) return this.pinBoard[i];
+			if ( this.pinBoard[i].id === id ) return this.pinBoard[i];
 		}
 		return false;
 	}
-    
-    /*
-	Model.prototype.updateMemo = function (memo){
-		if ( this.findMemo(memo) === false ) return false;
-		this.findMemo(memo).title = memo.title;
-		this.findMemo(memo).content = memo.content;
-    }
-    */
-    
-    Model.prototype.updateMemo = function (type, id, txt){
-        switch(type){
-            case "title":
-                for( var i in this.pinBoard ){
-                    if(id == this.pinBoard[i].id) this.pinBoard[i].title = txt;
-                }
-                break;
-            case "content":
-                for( var i in this.pinBoard ){
-                    if(id == this.pinBoard[i].id) this.pinBoard[i].content = txt;
-                }
-                break;
-        }
-    }
-                
-	Model.prototype.deleteMemo = function (memo){
-		this.pinBoard.splice(this.pinBoard.indexOf(this.findMemo(memo)),1);
+	Model.prototype.deleteMemo = function (id){
+		this.pinBoard.splice(this.pinBoard.indexOf(this.findMemo(id)),1);
 	}
-
-	//export to window
+	Model.prototype.updateMemo = function (type, id, txt){
+	    switch(type){
+	        case "title":
+	            for( var i in this.pinBoard ){
+	                if(id == this.pinBoard[i].id) this.pinBoard[i].title = txt;
+	            }
+	            break;
+	        case "content":
+	            for( var i in this.pinBoard ){
+	                if(id == this.pinBoard[i].id) this.pinBoard[i].content = txt;
+	            }
+	            break;
+	    }
+	}
 	window.app = window.app || {};
 	window.app.Model = Model;
 })(window);
-
-
-
-
-//view
-/* CONTENTEDITABLE CHANGE EVENT
-<div contenteditable="true" id="editor">Please type something in here</div>
-document.getElementById("editor").addEventListener("input", function() {
-    console.log("input event fired");
-}, false);
-*/
+//VIEW
 (function(window){
 	'use strict'
-
-    class View {
-        constructor(pinBoard) {
-            this.setView(pinBoard);
-        }
-    }
-
-    View.prototype.collectNodeArray = function (pinBoard){
-        var nodeArray = [];
-        for ( var i in pinBoard ){
-			var memoNode = document.getElementById(pinBoard[i].id);
-            var titleNode = memoNode.getElementsByClassName("title")[0];
-            var contentNode = memoNode.getElementsByClassName("content")[0];
-            var deleteButtonNode = memoNode.getElementsByClassName("delete")[0];
-            var hideButtonNode = memoNode.getElementsByClassName("hide")[0];
-            nodeArray.push({id:pinBoard[i].id, title:titleNode, content:contentNode, deleteButton:deleteButtonNode, hideButton:hideButtonNode});
-        }
-        return nodeArray;
-    }
-
-    View.prototype.setView = function (pinBoard){
-        var nodeArray = this.collectNodeArray(pinBoard);
-        for (var i in nodeArray){
-            var memoNode = nodeArray[i];
-            memoNode.title.innerHTML = pinBoard[i].title;
-            memoNode.title.contentEditable = true;
-            memoNode.content.innerHTML = pinBoard[i].content;
-            memoNode.content.contentEditable = true;
-        }
-    }
-    View.prototype.toggleHide = function (node){
-        var title = node.title;
-        var content = node.content;
-        if(content.style.display == 'none'){
-            title.style.display = 'block'
-            content.style.display = 'block';
-        } else {
-            title.style.display = 'none'
-            content.style.display = 'none';
-        }
-        
-    }
-        
-
-
-	View.prototype.addMemo = function (memo){  
-	}
-	View.prototype.findMemo = function (memo){
-	}
-	View.prototype.updateMemo = function (memo){
-	}
-	View.prototype.deleteMemo = function (memo){
-	}
-	View.prototype.loadpinBoard = function (memo){
-	}
-	View.prototype.savepinBoard = function (memo){
+	class View {
+		constructor() {}
 	}
 
+	View.prototype.createMemoElement = function(givenId, newId){
+		//create new memo node after given Id
+		var newNode = document.createElement("div");
+		newNode.setAttribute("class", "memo");
+		newNode.setAttribute("id", newId);
+		newNode.innerHTML =
+			"<div contentEditable = true class = \"title\">New Post</div><div contentEditable = true class = \"content\">Write Something!</div><input class=\"hide\" type=\"button\" value=\"hide\"><input class=\"add\" type=\"button\" value=\"add\"></div><input class=\"delete\" type=\"button\" value=\"delete\">";
+		if (givenId == null) {
+			var givenNode = document.getElementsByClassName("pinBoard")[0];
+			givenNode.appendChild(newNode);
+		}
+		else {
+			var givenNode = document.getElementById(givenId);
+			givenNode.parentNode.insertBefore(newNode, givenNode.nextSibling);
+		}
+		return newNode;
+	}
+	View.prototype.deleteMemo = function (targetNode){
+		targetNode.parentNode.removeChild(targetNode);
+	}
+	View.prototype.toggleHide = function (memoNode){
+    	var title = memoNode.getElementsByClassName("title")[0];
+    	var content = memoNode.getElementsByClassName("content")[0];
+    	if(content.style.display == 'none'){
+        	content.style.display = 'block';
+    	} else {
+        	content.style.display = 'none';
+    	}
 
-
-	//export to window
+	}
+	//Export to window
 	window.app = window.app || {};
 	window.app.View = View;
 })(window);
-
-
-
-
-
-//controller
+//CONTROLLER
 (function(window){
 	'use strict'
-    class Controller {
-        constructor(model, view, store) {
-            this.model = model;
-            this.view = view;
-            this.store = store;
-            this.autoSave = false;
-            this.bindEvent(this.model.pinBoard);
-        }
-    }
-    Controller.prototype.toggleAutoSave = function(){
-        if(this.autoSave) this.autoSave = false;
-        else this.autoSave = true;
-    }
-    
-	Controller.prototype.addMemo = function (memo){
-        
+	class Controller {
+	    constructor(model, view, store) {
+	        this.model = model;
+	        this.view = view;
+	        this.store = store;
+			var newNode = this.view.createMemoElement(null,"memo-default");
+			this.bindEvent(newNode);
+	    }
+	}
+	Controller.prototype.bindEvent = function (newNode){
+		var id = newNode.id;
+	    var titleNode = newNode.getElementsByClassName("title")[0];
+	    var contentNode = newNode.getElementsByClassName("content")[0];
+	    var hideButtonNode = newNode.getElementsByClassName("hide")[0];
+	    var addButtonNode = newNode.getElementsByClassName("add")[0];
+		var deleteButtonNode = newNode.getElementsByClassName("delete")[0];
+	    titleNode.addEventListener("input", this.updateMemo.bind(this, "title", id, titleNode));
+	    contentNode.addEventListener("input", this.updateMemo.bind(this, "content", id, contentNode));
+	    deleteButtonNode.addEventListener("click", this.deleteMemo.bind(this, newNode));
+	    addButtonNode.addEventListener("click", this.addMemo.bind(this, id));
+	    hideButtonNode.addEventListener("click", this.view.toggleHide.bind(this, newNode));
+	}
+
+	Controller.prototype.generateId = function(){
+		var id = "memo-" + Date.now() + Math.floor(Math.random() * 1000);
+		for (var i in this.model.pinBoard){
+			if ( this.model.pinBoard[i] == id ){
+				this.generateID();
+			}
+		}
+        return id;
+	}
+	Controller.prototype.addMemo = function (givenId){
+        var newId = this.generateId();
+        var memo = {id:newId,title:"",content:""};
+        this.model.addMemo(memo); //add memo into array at the next index of prevousId
+        var newNode = this.view.createMemoElement(givenId, memo.id); //add memo into node at the next to prevousId
+        this.bindEvent(newNode);
     }
     Controller.prototype.updateMemo = function (type, id, node){
         this.model.updateMemo(type, id, node.innerHTML);
-        if(this.autoSave) this.store.savePinBoard(this.model.pinBoard);        
-    }    
-	
-    Controller.prototype.bindEvent = function (pinBoard){
-        var nodeArray = this.view.collectNodeArray(pinBoard);
-        for (let i in nodeArray){
-            var id = nodeArray[i].id;
-            var titleNode = nodeArray[i].title;
-            var contentNode = nodeArray[i].content;
-            var hideButtonNode = nodeArray[i].hideButton;
-            titleNode.addEventListener("input", this.updateMemo.bind(this, "title", id, titleNode));
-            contentNode.addEventListener("input", this.updateMemo.bind(this, "content", id, contentNode));
-            //nodeArray[i].deleteButton.addEventListener("click", this.deleteMemo);
-            hideButtonNode.addEventListener("click", this.view.toggleHide.bind(this, nodeArray[i]));
-        }
-    }    
-    
-    
-        
-    
-
-    Controller.prototype.undoPinBoard = function (){
-        this.model.pinBoard = this.store.pinBoard;
-        this.setView(this.model.pinBoard);
     }
-	Controller.prototype.deleteMemo = function (id){
+	Controller.prototype.deleteMemo = function (newNode){
+	    this.model.deleteMemo(newNode.id);
+		this.view.deleteMemo(newNode);
 	}
-	Controller.prototype.readMemo = function (id){
-	}
-	Controller.prototype.writeMemo = function (id){
-	}
-
-
 	//export to window
 	window.app = window.app || {};
 	window.app.Controller = Controller;
 })(window);
 
-
-
-
-
-//app
 (function(window){
 	'use strict'
+ 	function DetectChange(storeArray, modelArray){
+		this.storeArray = []; // new Array container
+		for (var i in storeArray){
+		    this.storeArray[i] = Object.assign({}, storeArray[i]);
+		}
+		this.modelArray = []; // new Array container
+		for (var i in modelArray){
+		    this.modelArray[i] = Object.assign({}, modelArray[i]);
+		}
+			/*this.storeArray = Array.from(storeArray, x=>x);
+			this.modelArray = Array.from(modelArray, x=>x);*/
+	}
+	DetectChange.prototype.getDifferentItems = function (){
+		for (var i in this.modelArray)
+			for (var j in this.storeArray){
+				if (JSON.stringify(this.modelArray[i]) == JSON.stringify(this.storeArray[j])){
+				this.modelArray.splice(i,1);
+				this.storeArray.splice(j,1);
+				this.getDifferentItems();
+				}
+			}
+		return {store:this.storeArray, model:this.modelArray};
+	}
+	window.app = window.app || {};
+	window.app.DetectChange = DetectChange;
+})(window);
+/* testcode
+window.DetectChange = app.DetectChange(store.pinBoard, model.pinBoard);
+detectChange.getDifferentItems();
+*/
+
+//APP
+(function(window){
+	'use strict'
+
     window.store = new app.Store;
 	window.model = new app.Model(store.pinBoard);
-	window.view = new app.View(model.pinBoard);
-	window.controller = new app.Controller(model, view, store);
 
+	window.view = new app.View();
+	window.controller = new app.Controller(model, view, store);
 })(window);
